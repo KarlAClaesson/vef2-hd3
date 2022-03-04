89c265e31566a5052e629d8dcf9fd0b4725d7117
Karl <karlclaesson@protonmail.com>
2022-03-04 13:40:37 +0000

    testing app.js changes to run heroku

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 import express from 'express';
-const app = express();
-
-
 import { userRouter } from './routes/users.js';
-
 import { eventRouter } from './routes/events.js'
+
+
+const {
+    PORT: port = 3000,
+    SESSION_SECRET: sessionSecret,
+    DATABASE_URL: connectionString,
+  } = process.env;
+  
+if (!connectionString || !sessionSecret) {
+console.error('Vantar gögn í env');
+process.exit(1);
+}
+
+  const app = express();
 
 app.use(express.json());
 
